5b56a1514cb422434c4010da1ca502a54b2fa73c
Charles Clemens <cclemensjr@gmail.com>
2019-01-08 09:02:27 -0800

    Added logic to let the user know if they selected the right answer

--- AboutMeQuiz/Program.cs
+++ AboutMeQuiz/Program.cs
@@ -15,17 +15,26 @@ namespace AboutMeQuiz
         {
             Console.WriteLine("In the Play function");
             // The application must provide at least 5 different questions
+
+            // The first question deals with my training in martial arts. Martial arts have been in important part of my life for many years.
             Console.WriteLine("Question 1. Which martial art have I not studied?");
             Console.WriteLine("\ta. Brazilian Jujitsu");
             Console.WriteLine("\tb. Aikido");
             Console.WriteLine("\tc. Tae Kwan Do");
             Console.WriteLine("\td. Jeet Kune Do");
 
+            // Collecte user input
             string answer1 = Console.ReadLine();
-
-            Console.WriteLine($"You entered { answer1 }");
-
-                // The correct answer is Brazilian Jujitsu
+            
+            // Add some logic to let the user know if they were right or wrong
+            if (answer1.ToUpper() == "A")
+            {
+                Console.WriteLine("That is correct! I have trained in the arts of Aikido, Tae Kwan Do, and Jeet Kune Do but not Brazilian Jujitsu");
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry but that is incorrect. I have trained in the arts of Aikido, Tae Kwan Do, and Jeet Kune Do but not Brazilian Jujitsu");
+            }
 
     
             // Question 2. How many novels have I written?
