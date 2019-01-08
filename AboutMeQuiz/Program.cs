710d3d8e2b4bd84bbee6e5a3bd3f7769c8f1cf24
CClemensJr <cclemensjr@gmail.com>
2019-01-07 22:21:26 -0800

    Added comments explaining what Play() will do.

--- AboutMeQuiz/Program.cs
+++ AboutMeQuiz/Program.cs
@@ -6,9 +6,11 @@ namespace AboutMeQuiz
     {
         static void Main(string[] args)
         {
+            // Main will call the Play function to activate the came without any direct code manipulation.
             Play();
         }
 
+        // Play is the function that will call all of the other functions.
         static void Play()
         {
             Console.WriteLine("In the Play function");
